50261d4fd36aba9b6b122b7e8d19e7bc925bda4d
allenDG <allenwalterfiestada@gmail.com>
2025-07-05 00:13:07 +0800

    Feat: added layout for Home page

--- src/components/layouts/HomeLayout.tsx
+++ src/components/layouts/HomeLayout.tsx
@@ -1,18 +1,23 @@
 // src/layouts/HomeLayout.tsx
 import { Outlet } from "react-router-dom";
+import { useTheme } from "next-themes";
 import Navbar from "@/components/common/navigation/Navbar";
 import Footer from "@/components/layouts/FooterLayout";
 
 const HomeLayout = () => {
+  const { theme } = useTheme();
+
   return (
-    <div className="flex flex-col min-h-screen bg-white text-gray-900">
-      <Navbar />
+    <div className={`flex flex-col min-h-screen ${theme === "dark" ? "dark" : ""}`}>
+      <div className="flex flex-col min-h-screen bg-background text-foreground">
+        <Navbar />
 
-      <main className="flex-1 container mx-auto px-4 py-6">
-        <Outlet />
-      </main>
+        <main className="flex-1 container mx-auto px-4 py-6">
+          <Outlet />
+        </main>
 
-      <Footer />
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,10 @@
+import {HeroSection} from "@/pages/home/screens/HeroSection";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <main className="flex flex-col w-full">
+      <HeroSection/>
+      
+    </main>
+  );
 }
